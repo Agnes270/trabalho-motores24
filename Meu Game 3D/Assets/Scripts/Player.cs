63e1a8b79fa79e7cd61ef46a243b036286dcee31
Agnes270 <carvalho.cecilia@escolar.ifrn.edu.br>
2024-08-21 15:27:42 -0300

    salvar

--- Meu Game 3D/Assets/Scripts/Player.cs
+++ Meu Game 3D/Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using Vector3 = UnityEngine.Vector3;
 using System.Collections;
 using System.Collections.Generic;
@@ -9,31 +10,46 @@ using UnityEngine.SceneManagement;
 public class Player : MonoBehaviour
 {
     public int velocidade = 10;
+    public int forcapulo = 7;
+    private bool noChão;
+
     public Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("START");
+        Debug.Log("star");
         TryGetComponent(out rb);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!noChão && collision.gameObject.tag == "Chão")
+        {
+            noChão = true;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("UPDATE");
+        Debug.Log("Update");
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
 
-        Vector3 direcao = new UnityEngine.Vector3(x,0,y);
+        Vector3 direcao = new UnityEngine.Vector3(x, 0, y);
         rb.AddForce(direcao * velocidade * Time.deltaTime, ForceMode.Impulse);
+
+        if (Input.GetKeyDown(KeyCode.Space) && noChão)
+        {
+            rb.AddForce(Vector3.up * forcapulo, ForceMode.Impulse);
+            noChão = false;
+        }
 
         if (transform.position.y <= -10)
         {
 
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
-        
-        
-        
     }
 }
